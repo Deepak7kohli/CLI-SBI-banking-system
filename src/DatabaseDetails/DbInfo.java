edf226407b33e6bceba4cd7aaaa70945dba1cf51
Deepak Kumar <deepakdkay201617@gmail.com>
2026-07-18 03:15:15 +0530

    Remove IDE generated files

---
+++ src/DatabaseDetails/DbInfo.java
@@ -0,0 +1,9 @@
+package DatabaseDetails;
+
+public interface DbInfo {
+	String driver="com.mysql.cj.jdbc.Driver";
+	String url="jdbc:mysql://localhost:3306/BankingManagementSystem";
+	String uname="root";
+	String password="8126";
+
+}
